05ad12aa326ccea424a67a36998ece2b58ae04c5
daniel-tsv <jim.w2610@proton.me>
2024-09-12 21:26:09 +0300

    fix middleware blocking requests

--- app/lib/actions/auth.ts
+++ app/lib/actions/auth.ts
@@ -2,15 +2,13 @@
 
 import { log } from 'console';
 import { redirect } from 'next/navigation';
-import { NextResponse } from 'next/server';
 import type { UserRole } from '../definitions';
 import { createSession, deleteSession } from '../session';
 
 export async function login(userId: number, role: UserRole) {
   await createSession(userId, role);
   log('redirection to /');
-  return NextResponse.redirect('/login'); // TODO
-  //redirect('/');
+  redirect('/');
 }
 
 export async function logout() {
--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,14 @@ import { NextResponse, type NextRequest } from 'next/server';
 import { getSession, updateSession } from './app/lib/session';
 
 export async function middleware(request: NextRequest) {
+  console.log(request);
+
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith('/_next/') || pathname.startsWith('/static/')) {
+    return;
+  }
+
   const session = await getSession();
   if (!session && request.nextUrl.pathname !== '/login') {
     return NextResponse.redirect(new URL('/login', request.url));
