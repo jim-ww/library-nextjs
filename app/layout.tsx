aeecd637d385175074d9f1b83640fb483f3abaff
jim-ww <jim.w2610@proton.me>
2024-09-18 21:10:36 +0300

    wip: start implementing mobile menu for panel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Panel } from './ui/components/Panel';
+import { users } from './lib/mock_data';
+import type { User } from './lib/definitions';
 
 export const metadata: Metadata = {
   title: 'Next Library',
@@ -13,10 +15,14 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user: User | null = users[0]; // TODO await getCurrentUser();
+
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased lg:flex`}>
-        <Panel />
+      <body
+        className={`${inter.className} antialiased flex flex-col lg:flex-row`}
+      >
+        <Panel user={user} />
         {children}
       </body>
     </html>
--- app/ui/components/Panel.tsx
+++ app/ui/components/Panel.tsx
@@ -1,56 +1,104 @@
+'use client';
+
 import type { User } from '@/app/lib/definitions';
-import { users } from '@/app/lib/mock_data';
 import {
+  Bars3Icon,
   BuildingLibraryIcon,
   ChevronDownIcon,
+  ChevronLeftIcon,
   DocumentTextIcon,
   InboxStackIcon,
   Squares2X2Icon,
   UsersIcon,
 } from '@heroicons/react/20/solid';
 import Link from 'next/link';
-import type { ReactNode } from 'react';
+import { useState, type ReactNode } from 'react';
 
-export function Panel() {
-  const user: User | null = users[0]; // TODO await getCurrentUser();
+export function Panel({ user }: Readonly<{ user: User }>) {
+  const [mobileMenuClosed, setMobileMenuClosed] = useState(true);
+  const [userProfileClosed, setUserProfileClosed] = useState(true);
+
+  const iconSize = 'size-6';
 
   return (
-    <div className="bg-blue-950 whitespace-nowrap sticky top-0 lg:top-auto w-screen lg:w-auto lg:h-screen">
-      <div className="flex lg:flex-col items-center lg:items-start  lg:h-full">
-        <h1 className="text-white text-lg md:text-xl lg:text-2xl font-bold p-2 lg:p-5 ">
-          Next Library
-        </h1>
+    <>
+      <div className="bg-blue-950 whitespace-nowrap sticky top-0 lg:top-auto md:w-screen lg:w-auto lg:h-screen">
+        <div className="flex lg:flex-col items-center lg:items-start  lg:h-full">
+          {/* // * Mobile menu button  */}
+          <button
+            type="button"
+            aria-controls="menu"
+            aria-expanded="false"
+            className="md:hidden"
+            onClick={() => setMobileMenuClosed(!mobileMenuClosed)}
+          >
+            <Bars3Icon className={`${iconSize} text-white`} />
+          </button>
 
-        {user && (
-          <>
-            <NavBar />
+          <h1 className="text-white text-lg md:text-xl lg:text-2xl font-bold p-2 lg:p-5 lg:pb-4">
+            Next Library
+          </h1>
 
-            {/* Empty div to take up remaining space between NavBar and UserProfile */}
-            <div className="flex-grow" />
+          {user && (
+            <>
+              <NavBar classname="hidden md:inline" />
+              <div className="flex-grow" />
+              <UserProfileBubble
+                user={user}
+                toggleUserProfileMenu={() =>
+                  setUserProfileClosed(!userProfileClosed)
+                }
+              />
+            </>
+          )}
+        </div>
+      </div>
+      {mobileMenuClosed || (
+        <div className="bg-blue-950 whitespace-nowrap absolute left-0 top-16 bottom-0 p-2">
+          <NavBar />
+        </div>
+      )}
+      {userProfileClosed || <UserProfileOptions />}
+    </>
+  );
+}
 
-            {/* User Profile */}
-            <div className="flex flex-col bg-yellow-500 text-black p-4 lg:w-full">
-              <p className="font-semibold text-xs">
-                {(user.role + ' ROLE').toUpperCase()}
-              </p>
-              <div className="flex">
-                <span className="font-bold">{user.name}</span>
-                <ChevronDownIcon className="size-6 ml-auto" />
-              </div>
-            </div>
-          </>
-        )}
+function UserProfileBubble({
+  user,
+  toggleUserProfileMenu,
+}: Readonly<{ user: User; toggleUserProfileMenu: () => void }>) {
+  return (
+    <div id="menu" className="flex lg:flex-col lg:w-full">
+      {/* User Profile */}
+      <div className="flex flex-col bg-yellow-500 text-black p-4 lg:w-full">
+        <p className="font-semibold text-xs">
+          {(user.role + ' ROLE').toUpperCase()}
+        </p>
+        <button
+          type="button"
+          className="flex items-center"
+          onClick={toggleUserProfileMenu}
+        >
+          <span className="font-bold">{user.name}</span>
+          <ChevronDownIcon className={'size-6 ml-auto hidden md:inline'} />
+          <ChevronLeftIcon className={'size-6 inline md:hidden ml-1 '} />
+        </button>
       </div>
     </div>
   );
 }
 
-function NavBar() {
+function UserProfileOptions() {
+  // TODO implement
+  return <div>User profile options here...</div>;
+}
+
+function NavBar({ classname }: Readonly<{ classname?: string }>) {
   const itemIconClassname = 'size-6';
 
   return (
-    <nav>
-      <ul className="flex text-white lg:flex-col">
+    <nav className={classname ?? ''}>
+      <ul className="text-white flex flex-col md:flex-row lg:flex-col ">
         <ListItem link="/" text="Dashboard">
           <Squares2X2Icon className={itemIconClassname} />
         </ListItem>
@@ -84,10 +132,10 @@ function ListItem({
     <li>
       <Link
         href={link}
-        className=" hover:bg-cyan-800 rounded flex md:flex-col md:items-center lg:items-start lg:flex-row gap-1 md:gap-2 p-2 lg:pl-5 lg:pr-14"
+        className=" hover:bg-cyan-800 flex md:flex-col md:items-center lg:items-start lg:flex-row gap-1 md:gap-2 p-2 lg:pl-5 lg:pr-14"
       >
         {children}
-        <span className="hidden md:inline">{text}</span>
+        <span>{text}</span>
       </Link>
     </li>
   );
